674d9731601fe8e452ad589768665d276d0eece3
Nic <admin@Nics-MacBook-Pro.local>
2020-04-23 11:27:05 +0200

    Added import

--- Scene.js
+++ Scene.js
@@ -1,3 +1,5 @@
+//var THREE = require('three');
+import * as THREE from 'three';
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75 , window.innerWidth/window.innerHeight,0.1,1000);
 
