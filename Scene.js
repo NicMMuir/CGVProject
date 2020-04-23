b19d4e1f85b3fe3ead5c0c662b43bc7795a9ecc5
Nic <admin@Nics-MacBook-Pro.local>
2020-04-23 12:08:41 +0200

    Fixed run

--- Scene.js
+++ Scene.js
@@ -1,30 +1,24 @@
-//var THREE = require('three');
-import * as THREE from 'three';
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75 , window.innerWidth/window.innerHeight,0.1,1000);
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth/2 , window.innerHeight/2);
-document.body.appendChild(renderer.domElement);
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-//with perspective camera:
-//FOV (Field of view)
-//aspect ration = use (width of element / height)
-//far and near clipping pane clips render distances
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-//To test output:
-var geometry = new THREE.BoxGeometry(10,10,10);
-var material = new THREE.MeshBasicMaterial({color:0x00ff00});
-var cube = new THREE.Mesh(geometry,material);
-scene.add(cube);
 camera.position.z = 5;
 
-//to actually render the scene:
-function animate(){
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene,camera);
+var animate = function () {
+	requestAnimationFrame( animate );
 
-}
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+};
+
 animate();
