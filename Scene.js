3a90886aed697a9bfc9876f6be57ec3ec77d37b6
Nic <admin@Nics-MacBook-Pro.local>
2020-04-25 18:52:07 +0200

    Edited the file heavily with new phisics stuff, not working

--- Scene.js
+++ Scene.js
@@ -1,45 +1,142 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.set(0,3,20);
-camera.lookAt(0,0,0);
-camera.updateProjectionMatrix();
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
 
+//Everything up to this point is to render the first frame
 
-
-
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-cube.position.y += 0.5;
-scene.add( cube );
-
-
+var scene, camera, renderer, loop,cube,cubedata,controller ;
+var frame = 0;
 var xSpeed = 0.5;
 var zSpeed = 0.5;
-GenPlane();
-SetLight();
+var ySpeed = 0;
 
-renderer.render(scene,camera)
-document.addEventListener("keydown",onDocumentKeyDown, false);
-function onDocumentKeyDown(event) {
-
-    var keyCode = event.which;
-    if (keyCode == 87) {
-        cube.position.z -= zSpeed;
-    } else if (keyCode == 83) {
-        cube.position.z += zSpeed;
-    } else if (keyCode == 65) {
-        cube.position.x -= xSpeed;
-    } else if (keyCode == 68) {
-        cube.position.x += xSpeed;
-    } else if (keyCode == 32) {
-        cube.position.set(0, 0, 0);
-    }
-renderer.render(scene,camera)
+cubedata = {
+	W:1,
+	H:1,
+	B:1,
+	jump:true,
+	x_vel:0,
+	y_vel:0,
+	z_vel:0,
+	x:0,
+	y:0,
+	z:0
 };
+
+init();
+
+function init(){
+	scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+	renderer = new THREE.WebGLRenderer();
+
+	camera.position.set(0,3,20);
+	camera.lookAt(0,0,0);
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild(renderer.domElement );
+}
+
+
+function render(){
+	renderer.render(scene,camera);
+};
+
+
+
+controller = {
+
+		forward:false,
+		back:false,
+		left:false,
+		right:false,
+		up:false,
+
+keyListener:function(event){
+	var keystate = (event.type == "keydown")?true:false;
+	switch (event.keyCode) {
+		case 87:
+				controller.forward = keystate;
+			break;
+		case 83:
+				controller.back = keystate;
+			break;
+		case 65:
+				controller.left = keystate;
+			break;
+		case 68:
+				controller.right = keystate;
+			break;
+		case 32:
+				controller.up = keystate;
+			break;
+	}
+
+
+
+
+}
+
+// function CubeMove(event) {
+//     var keyCode = event.which;
+//
+//     if (keyCode == 87) {
+//         cube.position.z -= zSpeed;
+//     } else if (keyCode == 83) {
+//         cube.position.z += zSpeed;
+//     } else if (keyCode == 65) {
+//         cube.position.x -= xSpeed;
+//     } else if (keyCode == 68) {
+//         cube.position.x += xSpeed;
+//     } else if (keyCode == 32) {
+//         cube.position.set(0, 0.5, 0);
+//     }
+// };
+
+}
+
+loop = function(){
+
+	if(controller.up && cubedata.jump == false){
+		cubedata.yval -=5;
+		cubedata.jump = true;
+	}
+
+	if(controller.left){
+		cubedata.xval -=0.5;
+	}
+	if(controller.right){
+		cubedata.xval +=0.5;
+	}
+	if(controller.forward){
+		cubedata.zval -=0.5;
+	}
+	if(controller.back){
+		cubedata.zval +=0.5;
+	}
+
+	cubedata.y_vel +=1.5;//gravity
+	cubedata.x += cubedata.x_vel;
+	cubedata.z += cubedata.z_vel;
+	cubedata.y += cubedata.y_vel;
+	cubedata.x_vel *= 0.9;//friction
+	cubedata.y_vel *= 0.9;//friction
+	cubedata.z_vel *= 0.9;//friction
+
+	//colision detection
+	if(cubedata.y > -1){
+		cubedata.jump = false;
+		cubedata.y = -1;
+		cubedata.t_vel = 0;
+	}
+	GenPlane();
+	SetLight();
+	GenCube(cubedata.x,cubedata.y,cubedata.z);
+	render();
+  requestAnimationFrame(loop);
+
+}
+
+
+
+
 
 
 function GenPlane(){
@@ -50,7 +147,7 @@ function GenPlane(){
 	plane.rotation.x = Math.PI / 2;
 	scene.add( plane );
 
-}
+};
 
 
 function SetLight(){
@@ -58,5 +155,25 @@ function SetLight(){
 	scene.add(light);
 
 	var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.7 );
+	directionalLight.position.set( 0,4,1 );
+	directionalLight.castShadow = true;
 	scene.add( directionalLight );
+};
+
+function GenCube(x1,y1,z1){
+	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+	cube = new THREE.Mesh( geometry, material );
+	cube.position.x = x1;
+	cube.position.y = y1;
+	cube.position.z = z1;
+	scene.add(cube);
 }
+
+
+
+//Event Listeners:
+
+window.addEventListener("keydown",controller.keyListener);
+window.addEventListener("keyup",controller.keyListener);
+window.requestAnimationFrame(loop);
