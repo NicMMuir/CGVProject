65bfbf6756e05ab4fb05c2726ddd042d8b185def
Nic <admin@Nics-MacBook-Pro.local>
2020-05-06 20:33:54 +0200

    Camera movement with collisions working aswell as rotation of char

---
+++ Map1.js
@@ -0,0 +1,152 @@
+var ObjectsArr = [];
+
+
+var geometry1 = new THREE.BoxGeometry( 20, 1, 20 );
+var material1 = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+var geometry2 = new THREE.BoxGeometry( 22, 3, 22 );
+var material2 = new THREE.MeshBasicMaterial( { color: 0xFF0000 } );
+
+var geometry3 = new THREE.BoxGeometry( 22, 3, 3 );
+var material3 = new THREE.MeshBasicMaterial( { color: 0xFF00ff } );
+var geometry4 = new THREE.BoxGeometry( 22, 3, 10 );
+var material4 = new THREE.MeshBasicMaterial( { color: 0xFF0000 } );
+var geometry5 = new THREE.BoxGeometry( 22, 3, 5 );
+var material5 = new THREE.MeshBasicMaterial( { color: 0x808080 } );
+var geometry6 = new THREE.BoxGeometry( 7, 3, 5 );
+var material6 = new THREE.MeshBasicMaterial( { color: 0x464646 } );
+var geometry7 = new THREE.BoxGeometry( 14, 3, 5 );
+var geometry8 = new THREE.CircleGeometry( 10, 10, 10 );
+
+var floor1 = new THREE.Mesh( geometry1, material1 );
+var floor2 = new THREE.Mesh( geometry2, material2 );
+var floor3 = new THREE.Mesh( geometry2, material2 );
+var floor4 = new THREE.Mesh( geometry2, material2 );
+var floor5 = new THREE.Mesh( geometry2, material2 );
+var cubicLog1 = new THREE.Mesh( geometry3, material3 );
+var floor6 = new THREE.Mesh( geometry4, material4 );
+var floor7 = new THREE.Mesh( geometry4, material4 );
+//Steps begin:
+var floor8 = new THREE.Mesh( geometry5, material5 );
+var floor9 = new THREE.Mesh( geometry5, material5 );
+var floor10 = new THREE.Mesh( geometry5, material5 );
+var floor11 = new THREE.Mesh( geometry6, material6 );
+var floor12 = new THREE.Mesh( geometry6, material6 );
+var floor13 = new THREE.Mesh( geometry5, material5 );
+var floor14 = new THREE.Mesh( geometry5, material5 );
+var floor15 = new THREE.Mesh( geometry5, material5 );
+var floor16 = new THREE.Mesh( geometry7, material6 );
+var floor17 = new THREE.Mesh( geometry5, material5 );
+var floor18 = new THREE.Mesh( geometry5, material5 );
+var floor19 = new THREE.Mesh( geometry7, material6 );
+var floor20 = new THREE.Mesh( geometry5, material5 );
+//Steps end
+var floor21 = new THREE.Mesh( geometry2, material2 );
+var cubicLog2 = new THREE.Mesh( geometry3, material3 );
+var floor22 = new THREE.Mesh( geometry2, material2 );
+var floor23 = new THREE.Mesh( geometry2, material2 );
+var floor24 = new THREE.Mesh( geometry2, material2 );
+var cubicLog3 = new THREE.Mesh( geometry3, material3 );
+var floor25 = new THREE.Mesh( geometry8, material2 );
+
+
+function genarr(){
+  ObjectsArr.push(floor1);
+  ObjectsArr.push(floor2);
+  ObjectsArr.push(floor3);
+  ObjectsArr.push(floor4);
+  ObjectsArr.push(floor5);
+  ObjectsArr.push(cubicLog1);
+  ObjectsArr.push(floor6);
+  ObjectsArr.push(floor7);
+  ObjectsArr.push(floor8);
+  ObjectsArr.push(floor9);
+  ObjectsArr.push(floor10);
+  ObjectsArr.push(floor11);
+  ObjectsArr.push(floor12);
+  ObjectsArr.push(floor13);
+  ObjectsArr.push(floor14);
+  ObjectsArr.push(floor15);
+  ObjectsArr.push(floor16);
+  ObjectsArr.push(floor17);
+  ObjectsArr.push(floor19);
+  ObjectsArr.push(floor20);
+  ObjectsArr.push(floor21);
+  ObjectsArr.push(cubicLog2);
+  ObjectsArr.push(floor22);
+  ObjectsArr.push(floor23);
+  ObjectsArr.push(floor24);
+  ObjectsArr.push(cubicLog3);
+  ObjectsArr.push(floor25);
+}
+
+function moveobjects(){
+  floor2.position.y = -2;
+	floor2.position.z = -20;
+	floor3.position.x = -20;
+	floor4.position.x = +20;
+	floor5.position.z = -45;
+	floor5.position.y = -2;
+	cubicLog1.position.z = -40;
+	floor6.position.y = -4;
+	floor6.position.z = -65;
+	floor7.position.y = -4;
+	floor7.position.z = -85;
+	floor8.position.y = -2;
+	floor8.position.z = -90;
+	floor9.position.y = 1;
+	floor9.position.z = -95;
+	floor10.position.y = 4;
+	floor10.position.z = -100;
+	floor11.position.x = 7.5;
+	floor11.position.y = 7;
+	floor11.position.z = -100;
+	floor12.position.x = -7.5;
+	floor12.position.y = 7;
+	floor12.position.z = -100;
+	floor13.position.y = 7;
+	floor13.position.z = -105;
+	floor14.position.y = 10;
+	floor14.position.z = -105;
+	floor15.position.y = 13;
+	floor15.position.z = -110;
+	floor16.position.x = 4;
+	floor16.position.y = 16;
+	floor16.position.z = -110;
+	floor17.position.y = 16;
+	floor17.position.z = -115;
+	floor18.position.y = 19;
+	floor18.position.z = -115;
+	floor19.position.x = -4;
+	floor19.position.y = 22;
+	floor19.position.z = -115;
+	floor20.position.y = 22;
+	floor20.position.z = -120;
+	floor21.position.y = 25;
+	floor21.position.z = -128;
+	cubicLog2.position.y = 28;
+	cubicLog2.position.z = -128;
+	floor22.position.x = 7;
+	floor22.position.y = 25;
+	floor22.position.z = -158;
+	floor22.rotation.set(0, -Math.PI / 6, 0);
+	floor23.position.x = 22;
+	floor23.position.y = 25;
+	floor23.position.z = -180;
+	floor23.rotation.set(0, -40, 0);
+	floor24.position.x = 40;
+	floor24.position.y = 25;
+	floor24.position.z = -200;
+	floor24.rotation.set(0, -40, 0);
+	cubicLog3.position.x = 40;
+	cubicLog3.position.y = 28;
+	cubicLog3.position.z = -200;
+	cubicLog3.rotation.set(0,40, 0);
+	floor25.position.x = 55;
+	floor25.position.y = 25;
+	floor25.position.z = -200;
+}
+
+function getarr(){
+  moveobjects();
+  genarr();
+}
--- Scene.js
+++ Scene.js
@@ -14,13 +14,13 @@ var zSpeed = 0.5;
 var ySpeed = 0;
 
 var camstartx = 0;
-camstarty = 40;
-camstartz = 50;
+var camstarty = 15;
+var camstartz = 30;
 
 scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 renderer = new THREE.WebGLRenderer();
-document.body.appendChild( renderer.domElement );
+
 
 cubedata = {
 	W:1,
@@ -32,11 +32,14 @@ cubedata = {
 	z_vel:0,
 	x:0,
 	y:2,
-	z:0
+	z:0,
+	rotationy:0
 };
 
-
-const controls = new THREE.OrbitControls(camera, renderer.domElement);
+camera.position.set(camstartx,camstarty,camstartz);
+camera.lookAt(cubedata.x,cubedata.y,cubedata.z);
+camera.updateProjectionMatrix();
+var controls = new THREE.PointerLockControls(camera);
 
 //raycasting
 var rayforward = new THREE.Raycaster();
@@ -46,11 +49,6 @@ var rayleft = new THREE.Raycaster();
 var rayright = new THREE.Raycaster();
 
 // Direction Vectors
-var vecforward = new THREE.Vector3(0,0,-1);
-var vecbackward = new THREE.Vector3(0,0,1);
-var vecleft = new THREE.Vector3(-1,0,0);
-var vecright = new THREE.Vector3(1,0,0);
-var vecdown = new THREE.Vector3(0,-1,0);
 
 
 //camerarotation
@@ -68,13 +66,6 @@ var robj= new Array();
 
 
 
-var geometry1 = new THREE.BoxGeometry( 20, 1, 20 );
-var material1 = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-
-
-
-var geometry2 = new THREE.BoxGeometry( 22, 3, 22 );
-var material2 = new THREE.MeshBasicMaterial( { color: 0xFF0000 } );
 
 // cube1.position.x = 0;
 // cube1.position.y = y1;
@@ -83,47 +74,6 @@ var materialC = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 cube = new THREE.Mesh( geometryC, materialC );
 
 
-var geometry3 = new THREE.BoxGeometry( 22, 3, 3 );
-var material3 = new THREE.MeshBasicMaterial( { color: 0xFF00ff } );
-var geometry4 = new THREE.BoxGeometry( 22, 3, 10 );
-var material4 = new THREE.MeshBasicMaterial( { color: 0xFF0000 } );
-var geometry5 = new THREE.BoxGeometry( 22, 3, 5 );
-var material5 = new THREE.MeshBasicMaterial( { color: 0x808080 } );
-var geometry6 = new THREE.BoxGeometry( 7, 3, 5 );
-var material6 = new THREE.MeshBasicMaterial( { color: 0x464646 } );
-var geometry7 = new THREE.BoxGeometry( 14, 3, 5 );
-var geometry8 = new THREE.CircleGeometry( 10, 10, 10 );
-
-var floor1 = new THREE.Mesh( geometry1, material1 );
-var floor2 = new THREE.Mesh( geometry2, material2 );
-var floor3 = new THREE.Mesh( geometry2, material2 );
-var floor4 = new THREE.Mesh( geometry2, material2 );
-var floor5 = new THREE.Mesh( geometry2, material2 );
-var cubicLog1 = new THREE.Mesh( geometry3, material3 );
-var floor6 = new THREE.Mesh( geometry4, material4 );
-var floor7 = new THREE.Mesh( geometry4, material4 );
-//Steps begin:
-var floor8 = new THREE.Mesh( geometry5, material5 );
-var floor9 = new THREE.Mesh( geometry5, material5 );
-var floor10 = new THREE.Mesh( geometry5, material5 );
-var floor11 = new THREE.Mesh( geometry6, material6 );
-var floor12 = new THREE.Mesh( geometry6, material6 );
-var floor13 = new THREE.Mesh( geometry5, material5 );
-var floor14 = new THREE.Mesh( geometry5, material5 );
-var floor15 = new THREE.Mesh( geometry5, material5 );
-var floor16 = new THREE.Mesh( geometry7, material6 );
-var floor17 = new THREE.Mesh( geometry5, material5 );
-var floor18 = new THREE.Mesh( geometry5, material5 );
-var floor19 = new THREE.Mesh( geometry7, material6 );
-var floor20 = new THREE.Mesh( geometry5, material5 );
-//Steps end
-var floor21 = new THREE.Mesh( geometry2, material2 );
-var cubicLog2 = new THREE.Mesh( geometry3, material3 );
-var floor22 = new THREE.Mesh( geometry2, material2 );
-var floor23 = new THREE.Mesh( geometry2, material2 );
-var floor24 = new THREE.Mesh( geometry2, material2 );
-var cubicLog3 = new THREE.Mesh( geometry3, material3 );
-var floor25 = new THREE.Mesh( geometry8, material2 );
 
 
 
@@ -133,149 +83,21 @@ init();
 function init(){
 
 
-	camera.position.set(camstartx,camstarty,camstartz);
-	camera.lookAt(0,0,0);
-	camera.updateProjectionMatrix();
+
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild(renderer.domElement );
 
+	getarr();
+	for(let k =0 ;k<ObjectsArr.length;k++){
+		scene.add( ObjectsArr[k] );
+		Collidables.push(ObjectsArr[k]);
+	}
+
+	controls.getObject().add(cube);
 
 
-
-
-
-
-
-
-
-
-	floor2.position.y = -2;
-	floor2.position.z = -20;
-	floor3.position.x = -20;
-	floor4.position.x = +20;
-	floor5.position.z = -45;
-	floor5.position.y = -2;
-	cubicLog1.position.z = -40;
-	floor6.position.y = -4;
-	floor6.position.z = -65;
-	floor7.position.y = -4;
-	floor7.position.z = -85;
-	floor8.position.y = -2;
-	floor8.position.z = -90;
-	floor9.position.y = 1;
-	floor9.position.z = -95;
-	floor10.position.y = 4;
-	floor10.position.z = -100;
-	floor11.position.x = 7.5;
-	floor11.position.y = 7;
-	floor11.position.z = -100;
-	floor12.position.x = -7.5;
-	floor12.position.y = 7;
-	floor12.position.z = -100;
-	floor13.position.y = 7;
-	floor13.position.z = -105;
-	floor14.position.y = 10;
-	floor14.position.z = -105;
-	floor15.position.y = 13;
-	floor15.position.z = -110;
-	floor16.position.x = 4;
-	floor16.position.y = 16;
-	floor16.position.z = -110;
-	floor17.position.y = 16;
-	floor17.position.z = -115;
-	floor18.position.y = 19;
-	floor18.position.z = -115;
-	floor19.position.x = -4;
-	floor19.position.y = 22;
-	floor19.position.z = -115;
-	floor20.position.y = 22;
-	floor20.position.z = -120;
-	floor21.position.y = 25;
-	floor21.position.z = -128;
-	cubicLog2.position.y = 28;
-	cubicLog2.position.z = -128;
-	floor22.position.x = 7;
-	floor22.position.y = 25;
-	floor22.position.z = -158;
-	floor22.rotation.set(0, -Math.PI / 6, 0);
-	floor23.position.x = 22;
-	floor23.position.y = 25;
-	floor23.position.z = -180;
-	floor23.rotation.set(0, -40, 0);
-	floor24.position.x = 40;
-	floor24.position.y = 25;
-	floor24.position.z = -200;
-	floor24.rotation.set(0, -40, 0);
-	cubicLog3.position.x = 40;
-	cubicLog3.position.y = 28;
-	cubicLog3.position.z = -200;
-	cubicLog3.rotation.set(0,40, 0);
-	floor25.position.x = 55;
-	floor25.position.y = 25;
-	floor25.position.z = -200;
-
-
-	scene.add( floor1 );
-	scene.add( floor2 );
-	scene.add( floor3 );
-	scene.add( floor4 );
-	scene.add( floor5 );
-	scene.add( cubicLog1 );
-	scene.add( floor6 );
-	scene.add( floor7 );
-	scene.add( floor8 );
-	scene.add( floor9 );
-	scene.add( floor10 );
-	scene.add( floor11 );
-	scene.add( floor12 );
-	scene.add( floor13 );
-	scene.add( floor14 );
-	scene.add( floor15 );
-	scene.add( floor16 );
-	scene.add( floor17 );
-	scene.add( floor18 );
-	scene.add( floor19 );
-	scene.add( floor20 );
-	scene.add( floor21 );
-	scene.add( cubicLog2 );
-	scene.add( floor22 );
-	scene.add( floor23 );
-	scene.add( floor24 );
-	scene.add( cubicLog3 );
-	scene.add( floor25 );
-	Collidables.push(floor1);
-	Collidables.push(floor2);
-	Collidables.push(floor3);
-	Collidables.push(floor4);
-	Collidables.push(floor5);
-	Collidables.push(cubicLog1);
-	Collidables.push(floor6);
-	Collidables.push(floor7);
-	Collidables.push(floor8);
-	Collidables.push(floor9);
-	Collidables.push(floor10);
-	Collidables.push(floor11);
-	Collidables.push(floor12);
-	Collidables.push(floor13);
-	Collidables.push(floor14);
-	Collidables.push(floor15);
-	Collidables.push(floor16);
-	Collidables.push(floor17);
-	Collidables.push(floor19);
-	Collidables.push(floor20);
-	Collidables.push(floor21);
-	Collidables.push(cubicLog2);
-	Collidables.push(floor22);
-	Collidables.push(floor23);
-	Collidables.push(floor24);
-	Collidables.push(cubicLog3);
-	Collidables.push(floor25);
-	scene.add(cube);
-
+	scene.add(controls.getObject());
 	SetLight();
-
-
-
 }
 
 
@@ -314,38 +136,27 @@ keyListener:function(event){
 	}
 }
 
-// function CubeMove(event) {
-//     var keyCode = event.which;
-//
-//     if (keyCode == 87) {
-//         cube.position.z -= zSpeed;
-//     } else if (keyCode == 83) {
-//         cube.position.z += zSpeed;
-//     } else if (keyCode == 65) {
-//         cube.position.x -= xSpeed;
-//     } else if (keyCode == 68) {
-//         cube.position.x += xSpeed;
-//     } else if (keyCode == 32) {
-//         cube.position.set(0, 0.5, 0);
-//     }
-// };
-
 }
 
 loop = function(){
 	distanceprev = cubedata.y;
-	GenCube(cubedata.x,cubedata.y,cubedata.z);
-	colisiondetection(cube);
+	MoveCube(cubedata.x,cubedata.y,cubedata.z);
+	colisiondetection(controls.getObject());
 	if(controller.up && cubedata.jump == false){
-		cubedata.y_vel +=7;
+		//must be a mutiple of the gravity
+		cubedata.y_vel +=10;
 		cubedata.jump = true;
 	}
 
 	if(controller.left){
+		// cube.rotation.y-=0.04;
+		// cubedata.rotationy -= 0.04
 		cubedata.x_vel -=0.04;
 	}
 	if(controller.right){
 		cubedata.x_vel +=0.04;
+		// cube.rotation.y +=0.04;
+		// cubedata.rotationy += 0.04
 	}
 	if(controller.forward){
 		cubedata.z_vel -=0.04;
@@ -354,24 +165,15 @@ loop = function(){
 		cubedata.z_vel +=0.04;
 	}
 
-	cubedata.y_vel -=0.3;//gravity
-	cubedata.x += cubedata.x_vel;
-	cubedata.z += cubedata.z_vel;
+	cubedata.y_vel -=0.25;//gravity
+	// cubedata.x += cubedata.x_vel;
+	// cubedata.z += cubedata.z_vel;
 	cubedata.y += cubedata.y_vel;
 	cubedata.x_vel *= 0.9;//friction
 	cubedata.y_vel *= 0.9;//friction
 	cubedata.z_vel *= 0.9;//friction
 
-	//colision detection
-	// if(cubedata.y < 1){
-	// 	cubedata.jump = false;
-	// 	cubedata.y = 1;
-	// 	cubedata.t_vel = 0;
-	// }
 
-	// down collis
-	//console.log(fobj);
-	  //fall through map
 if(dobj.length != 0){
 	if(cubedata.jump == true && dobj[0].distance<1.5 && distanceprev>cubedata.y){
 			cubedata.jump = false;
@@ -396,28 +198,28 @@ if(dobj.length != 0){
 	if(fobj.length != 0){
 		if(fobj[0].distance <= 1){
 			cubedata.jump = false;
-			cubedata.z = fobj[0].point.z+1.1;
+			controls.getObject().position.z = fobj[0].point.z-1.1;
 			//cubedata.t_vel = 0;
 		}
 	}
 	if(bobj.length != 0){
 		if(bobj[0].distance <= 1){
 			cubedata.jump = false;
-			cubedata.z = bobj[0].point.z-1.1;
+			controls.getObject().position.z = bobj[0].point.z+1.1;
 			//cubedata.t_vel = 0;
 		}
 	}
 	if(lobj.length != 0){
 		if(lobj[0].distance <= 1){
 			cubedata.jump = false;
-			cubedata.x = lobj[0].point.x+1.1;
+			controls.getObject().position.x = lobj[0].point.x+1.1;
 			//cubedata.t_vel = 0;
 		}
 	}
 	if(robj.length != 0){
 		if(robj[0].distance <= 1){
 			cubedata.jump = false;
-			cubedata.x = robj[0].point.x-1.1;
+			controls.getObject().position.x = robj[0].point.x+1.1;
 			//cubedata.t_vel = 0;
 		}
 	}
@@ -428,23 +230,49 @@ if(dobj.length != 0){
 
 //
 
-	GenCube(cubedata.x,cubedata.y,cubedata.z);
-	//mouserotate.update();
-	movecam();
+	MoveCube(cubedata.x,cubedata.y,cubedata.z);
+
+
 	render();
   window.requestAnimationFrame(loop);
 
 
 }
 
+
 function colisiondetection(cube){
-		cubevec.set(cubedata.x,cubedata.y,cubedata.z);
+
+	let realRot = controls.getObject().rotation.y;
+	let screenRot;
+	 if(controls.getObject().position.z == 0) {
+			 screenRot = Math.PI / 2 * Math.sign(controls.getObject().position.x);
+	 } else {
+			 if(controls.getObject().position.x != 0) {
+					 screenRot = Math.atan(controls.getObject().position.x / controls.getObject().position.z);
+			 } else {
+					 if(controls.getObject().position.z == -1) {
+							 screenRot = Math.PI;
+					 } else {
+							 screenRot = 0;
+					 }
+			 }
+	 }
+		let rot = (Math.PI + realRot) + screenRot;
+
+		 //rays in order to check for collisions
+		 var forw = new THREE.Vector3(Math.sin(rot), 0, -Math.cos(rot)); //Forward
+		 var backw  = new THREE.Vector3(Math.sin(rot), 0, Math.cos(rot)); //back
+		 var left = new THREE.Vector3(Math.sin(rot - Math.PI / 2), 0, Math.cos(rot - Math.PI / 2)); //Left
+		 var right = new THREE.Vector3(Math.sin(rot + Math.PI / 2), 0, Math.cos(rot + Math.PI / 2)); //Right
+		 var downw = new THREE.Vector3(0, -1, 0); //Down
+
+		cubevec.set(controls.getObject().position.x,controls.getObject().position.y,controls.getObject().position.z);
 		//raycaster.set(pos , direc);
-		rayforward.set(cubevec , vecforward);
-		raybackward.set(cubevec , vecbackward);
-		raydown.set(cubevec , vecdown);
-		rayleft.set(cubevec , vecleft);
-		rayright.set(cubevec , vecright);
+		rayforward.set(cubevec , forw);//farward
+		raybackward.set(cubevec , backw);//back
+		raydown.set(cubevec , downw);//down
+		rayleft.set(cubevec , left);//left
+		rayright.set(cubevec , right);//right
 		//.intersectObjects ( objects : Array, recursive : Boolean, optionalTarget : Array ) : Array
 		fobj = rayforward.intersectObjects(Collidables);
 		bobj = raybackward.intersectObjects(Collidables);
@@ -465,26 +293,30 @@ function SetLight(){
 	scene.add( directionalLight );
 };
 
-function GenCube(x1,y1,z1){
-	cube.position.x = x1;
-	cube.position.y = y1;
-	cube.position.z = z1;
-}
 
-const direction = new THREE.Vector3();
-const camOffset = 10;
-
-function movecam(){
-
-	camera.position.set(camera.position.x+cubedata.x_vel,camera.position.y,camera.position.z+cubedata.z_vel)
-	controls.target.set(cubedata.x,cubedata.y,cubedata.z);
-	controls.update();
+function MoveCube(x1,y1,z1){
+	controls.getObject().translateZ(cubedata.z_vel);
+	controls.getObject().translateX(cubedata.x_vel);
+	controls.getObject().position.y=cubedata.y;
 }
 
 
+
+//rotate Vectors
+
+function rotatevec(vec , angle){
+	vector.applyAxisAngle( axis, angle );
+}
+
+function OnMouseDown(event){
+	if(!controls.isLocked){
+		controls.lock();
+	}
+}
 
 //Event Listeners:
 
 window.addEventListener("keydown",controller.keyListener);
 window.addEventListener("keyup",controller.keyListener);
+window.addEventListener("mousedown",OnMouseDown,false);
 window.requestAnimationFrame(loop);
