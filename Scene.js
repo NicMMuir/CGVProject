2c96ef2747134eee44189117f513207fbf167151
Anthony Kubeka <nmkubeka@gmail.com>
2020-05-07 14:52:36 +0200

    removed intro menu till I can get it working

--- Scene.js
+++ Scene.js
@@ -5,12 +5,19 @@
 //Heightmap
 //orbital controls
 
-//---pause menu functionality------//
-var gamePaused = true; //for pause menu 
-window.onload=function(){
-	document.getElementById("startBtn").addEventListener("click", init);
-	
-}
+//---pause menu functionality (currenlty broken)------//
+/*var gamePaused = true; //for pause menu 
+document.getElementById("menu").style.display = "block"; 
+
+window.onload=function(){document.getElementById("startBtn").addEventListener("click", hideMenu); }
+
+function hideMenu(){
+//hides pause menu
+	gamePaused = false; 
+	document.getElementById("menu").style.display = "none"; 
+	gamePaused = false; 
+	console.log("started!");
+}*/
 
 
 var scene, camera,raycamera, renderer, loop,cube,cubedata,controller;
@@ -86,13 +93,10 @@ cube = new THREE.Mesh( geometryC, materialC );
 
 
 
-//init();
+init();
 
 function init(){
-	//hide pause menu 
-	document.getElementById("menu").style.display = "none"; 
-	gamePaused = false; 
-	console.log("started!");
+	
 
 
 
