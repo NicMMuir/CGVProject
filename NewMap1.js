a4b4fd453f23ead3b615acc0bcdb7c0b4b1459b4
Sultan <Imi-Sultan@users.noreply.github.com>
2020-05-15 22:28:55 +0200

    Ocean effect
    Added an Ocean effect  to help blend map into skybox

--- NewMap1.js
+++ NewMap1.js
@@ -1,10 +1,17 @@
 var ObjectsMap1Arr = [];
 
+var camera, controls, scene, renderer, stats;
+var Mesh, oceanGeometry, oceanMaterial, clock;
+
 //Textures
 var MainFloortexture = new THREE.TextureLoader().load( 'Textures/Grass.jpg' );
 var MainFloormaterial = new THREE.MeshBasicMaterial( { map: MainFloortexture, side: THREE.DoubleSide } );
 var Startpadtexture = new THREE.TextureLoader().load( 'Textures/start.jpg' );
 var Startpadmaterial = new THREE.MeshBasicMaterial( { map: Startpadtexture, side: THREE.DoubleSide } );
+var waterTexture = new THREE.TextureLoader().load( 'Textures/water.jpg' );
+var waterMat = new THREE.MeshBasicMaterial( { map: waterTexture } );
+
+
 
 MainFloortexture.wrapS = THREE.RepeatWrapping;
 MainFloortexture.wrapT = THREE.RepeatWrapping;
@@ -21,12 +28,34 @@ var NorthEastSeg = new THREE.Mesh( NorthEastSegmentgeom , MainFloormaterial );
 var NorthWestSeg = new THREE.Mesh( NorthWestSegmentgeom , MainFloormaterial );
 
 
+var oceanGeometry = new THREE.PlaneBufferGeometry( 20000, 20000, 128-1, 128-1);
+        oceanGeometry.rotateX( - Math.PI / 2 );
+
+        var position = oceanGeometry.attributes.position;
+        position.usage = THREE.DynamicDrawUsage;
+
+        for ( var i = 0; i < position.count; i ++ ) {
+
+          var y = 5 * Math.sin( i / 2 );
+          position.setY( i, y );
+
+        }
+
+var texture = new THREE.TextureLoader().load( 'textures/water.jpg' );
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+        texture.repeat.set( 5, 5 );
+
+var oceanMaterial = new THREE.MeshBasicMaterial( { color: 0x0044ff, map: texture } );
+var oceanMesh = new THREE.Mesh( oceanGeometry, oceanMaterial );
+
 
 function genarrMap1(){
+  
   ObjectsMap1Arr.push(startpad);
   ObjectsMap1Arr.push(SotheSeg);
   ObjectsMap1Arr.push(NorthEastSeg);
   ObjectsMap1Arr.push(NorthWestSeg);
+  ObjectsMap1Arr.push(oceanMesh);
 }
 function moveobjectsMap1(){
   //startpad.position.y = 2;
@@ -41,6 +70,8 @@ function moveobjectsMap1(){
 
   NorthWestSeg.position.z = -260;
   NorthWestSeg.position.x = -250;
+
+  oceanMesh.position.y = -10;
 }
 
 
@@ -49,3 +80,17 @@ function getarrMap1(){
   moveobjectsMap1();
   genarrMap1();
 }
+
+
+//   //Load SandHill Model
+//   var loader = new THREE.GLTFLoader();
+//   loader.load('./SandHill/scene.gltf', function(gltf){
+
+//     var temp = gltf.scene;
+//     // var mesh = temp.children[3];
+//       temp.position.x = 20;
+//       // ObjectsMap1Arr.push(temp);
+//       scene.add(temp);
+
+
+// })
